240cf29ab2390f2865a0cd0bf3fa696754b26ad1
Jun Loke <rebecca.j.loke@gmail.com>
2020-11-24 23:01:28 -0500

    Add prop validation

--- frontend/src/createStream/StreamingPage.js
+++ frontend/src/createStream/StreamingPage.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Box, Container } from "@material-ui/core";
+import PropTypes from "prop-types";
+
 import StreamingTable from "./StreamingTable";
 import DynamicBreadcrumb from "../General/DynamicBreadcrumb";
 import StreamList from "../streamlist/StreamList";
+import DeviceInfo from "../model/DeviceInfo";
+import StreamInfo from "../model/StreamInfo";
 
 export default class StreamingPage extends React.Component {
   constructor(props) {
@@ -35,3 +39,10 @@ export default class StreamingPage extends React.Component {
     );
   }
 }
+
+DeviceDetailsConciseTable.propTypes = {
+  deviceDataSource: PropTypes.arrayOf(PropTypes.instanceOf(DeviceInfo))
+    .isRequired,
+  streamDataSource: PropTypes.arrayOf(PropTypes.instanceOf(StreamInfo))
+    .isRequired
+};
